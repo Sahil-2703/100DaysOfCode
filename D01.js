46d2f3be6e014819d146723f6328664e1ecb2727
Sahil-2703 <117385977+Sahil-2703@users.noreply.github.com>
2023-09-11 19:46:12 +0530

    Day 1 of 100DaysOfCode challenge

---
+++ D01.js
@@ -0,0 +1,72 @@
+//Hello World program---
+// console.log("Hello World"); 
+
+// ************************************************************************************
+
+//variables in js
+// let a = 67;
+// console.log(a)
+// a = "Sahil"
+// console.log(a)
+// {
+//     let a = 56;
+//     console.log(a)
+// } 
+
+// const sahil = 45;
+// console.log(sahil)
+
+// ************************************************************************************
+
+// primitive data types
+// null number symbol string boolean bigint undefiend
+
+let a = null;
+let b = 45;
+let c = true;
+let d = BigInt('3479469284')
+let e = Symbol('I am a nice symbol ')
+let f = 'string'
+let g = undefined;
+
+// console.log(a, b, c, d, e, f, g)
+
+// console.log(typeof a)
+
+// objects ---> non primitive
+const items = {
+    name:"sahil",
+    age:19,
+    designaation:"student",
+    id:1
+}
+// console.log(items)
+
+let name = "Sahil"
+// console.log(name + 1)
+items['height'] = 34
+// console.log(items)
+
+// ************************************************************************************
+
+// Operators in Js
+// + - * / % ** ++ --
+// comparison ---> ===, ==, !=, !==, <, >,  
+
+// ************************************************************************************
+
+//expressions
+let num = 23523;
+// if(num>10){
+//     num++;
+//     console.log(num)
+// }
+// else{
+//     num--;
+//     console.log(num)
+// }
+let ansh = '5'
+// console.log(typeof ansh)
+ansh = Number.parseInt(ansh) // convert string to number
+// console.log(typeof ansh)
+
