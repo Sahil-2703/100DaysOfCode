0cd0f2f6730f8e2160688fd41773faa552f4dfb3
Sahil-2703 <117385977+Sahil-2703@users.noreply.github.com>
2023-09-12 19:13:14 +0530

    Day 2 of 100DaysOfCode

---
+++ D02.js
@@ -0,0 +1,25 @@
+// Conditional expression 
+let a = 10;
+if(a>5){
+    // console.log("this is age of greater than 5")
+}
+else{
+    // console.log("this is age of less than 5")
+}
+
+switch (a) {
+    case a<10:
+        // console.log("a is less than 10")
+        break;
+    
+    case a=10:
+        // console.log("a = 10")
+        break;
+
+    default:
+        break;
+}
+
+// a < 5 ? console.log("a < 5") : console.log("a > 5")
+
+
