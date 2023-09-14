5028fdde3e5b00ce858b16532ffb025d0c33b54e
Sahil-2703 <117385977+Sahil-2703@users.noreply.github.com>
2023-09-14 12:07:14 +0530

    Day 4 of 100DaysOfCode challenge

---
+++ D04.js
@@ -0,0 +1,45 @@
+// Strings
+let name = "Sahil";
+// console.log(name.length)
+for(a of name){
+    // console.log(a)
+}
+
+let c = 'pratik'
+let b = 'nilesh'
+let sentance = `c is friend of b`
+// console.log(sentance)
+
+let fruit = 'banana\'s\nthis is very tasty'
+// console.log(fruit)
+
+// methods
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.slice(1,4))
+// console.log(name.slice(2))
+// console.log(name.replace("Sah", "Soh"))
+let name2 = "           abc             "
+// console.log(name2.trim())
+
+for(i=0;i<=name.length;i++){
+    // console.log(name[i]);
+}
+
+
+const a1 = [
+    {
+        name:'Som',
+        roll:'151',
+        stream:'B.Tech'
+    },
+    {
+        name:'Sahil',
+        roll:'122',
+        stream:'B.Tech'
+    }
+]
+
+// console.log(a1)
+
+
