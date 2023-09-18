304bb8f11131c9bfc9c0a456484264586016bd06
Sahil-2703 <117385977+Sahil-2703@users.noreply.github.com>
2023-09-18 12:06:03 +0530

    Day 8 of 100DaysOfCOde challenge

---
+++ D08/D08.js
@@ -0,0 +1,15 @@
+// console.log("hello this is a console ")
+const obj = {
+    a:1,
+    b:2,
+    c:3,
+    d:4
+}
+
+//console.assert(55>3)
+// console.warn("this is warning")
+// console.error("this is error")
+// console.table(obj)
+
+// console.time("a")
+// console.timeEnd("a")
