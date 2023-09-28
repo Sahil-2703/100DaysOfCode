9b3de1f6dfe1ab78272a255740db505af273bd76
Sahil-2703 <117385977+Sahil-2703@users.noreply.github.com>
2023-09-28 19:58:00 +0530

    Day 16 of 100DaysOfCode challenge

---
+++ D16/D16.js
@@ -0,0 +1,9 @@
+console.log(document.body.firstElementChild)
+console.log(document.body.lastChild)
+console.log(document.body.firstChild)
+console.log(document.body.childNodes)
+console.log(document.body.childNodes[0])
+
+let t = document.body.firstElementChild.firstElementChild
+console.log(t.rows)
+console.log(t.tHead)
